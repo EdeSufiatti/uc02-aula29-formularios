eda66a3fdea46109e6f8f027780d7c9941a6588d
Ede Sufiatti <sufiattie@gmail.com>
2024-10-03 21:34:42 -0300

    c3

--- script.js
+++ script.js
@@ -5,22 +5,46 @@ const inputNome = document.getElementById("name");
 const inputEmail = document.getElementById("email");
 const inputSenha = document.getElementById("password");
 
+
+const listaPessoas = []
+
 function cadastrar(event) {
-  event.preventDefault()
-    
+    event.preventDefault()
 
 
-const nome = inputNome.value
-const email = inputEmail.value
-const senha = inputSenha.value
+    //obtendo dados do formulário para as consts
+    const nome = inputNome.value
+    const email = inputEmail.value
+    const senha = inputSenha.value
 
-console.log(
-    {
-        nome:nome,
-        email:email,
-        senha:senha,
+    //validações, verificando se os campos tem conteudo
+    if (!nome) {
+        alert("Campo nome não cadastrado, informe seu nome!")
+        inputNome.focus
+        return
+
     }
-)
+    if (!email) {
+        alert("Campo e-mail não cadastrado, informe se e-mail")
+        inputEmail.focus
+        return
+
+    }
+    if (!senha) {
+        alert("Campo Senha não cadastrado, informe a sua senha")
+        inputSenha.focus
+        return
+
+    }
+
+
+    console.log(
+        {
+            nome: nome,
+            email: email,
+            senha: senha,
+        }
+    )
 
 }
 
