a69515ba4330b8ee48019ca43bfbda9ed085a251
Ede Sufiatti <C:/Users/edemi>
2024-10-04 09:42:18 -0300

    commit 2 04 10 24

--- script.js
+++ script.js
@@ -1,52 +1,91 @@
-const formulario = document.getElementById("form")
+
+const formulario = document.getElementById("form");
 
 const btnCadastrar = document.getElementById("btnCadastrar")
-const inputNome = document.getElementById("name");
-const inputEmail = document.getElementById("email");
-const inputSenha = document.getElementById("password");
 
+const inputNome = document.getElementById("name")
+const inputEmail = document.getElementById("email")
+const inputSenha = document.getElementById("password")
+
+const tableListaPessoas = document.getElementById("listaPessoas")
 
 const listaPessoas = []
 
+// função que trata o evento onClick do botão cadastrar
 function cadastrar(event) {
-    event.preventDefault()
+  event.preventDefault()
+  // Obtendo os dados do formulário
+  const nome = inputNome.value
+  const email = inputEmail.value
+  const senha = inputSenha.value
 
+  // Validação, verificando se os campos tem conteúdo
+  if (!nome.trim()) {
+    alert("Informe seu nome!")
+    inputNome.focus()
+    return
+  }
 
-    //obtendo dados do formulário para as consts
-    const nome = inputNome.value
-    const email = inputEmail.value
-    const senha = inputSenha.value
+  if (!email.trim()) {
+    alert("Informe seu email!")
+    inputEmail.focus()
+    return
+  }
 
-    //validações, verificando se os campos tem conteudo
-    if (!nome) {
-        alert("Campo nome não cadastrado, informe seu nome!")
-        inputNome.focus
-        return
+  if (!(senha.trim())) {
+    alert("Informe uma senha!")
+    inputSenha.focus()
+    return
+  }
 
+  // Caso os dados estejam válidos, procura na lista 
+  // se já existe um cadastro com o mesmo email
+  let pessoaEncontrada = null
+  
+  // pesquisa com o for/each
+  // for (pessoa of listaPessoas) {
+  //   if (pessoa.email === email.toLowerCase()) {
+  //     pessoaEncontrada = pessoa
+  //     break
+  //   }
+  // }
+  // find
+  pessoaEncontrada = listaPessoas.find((pessoa) => {
+    if (pessoa.email === email.toLowerCase()){
+      return pessoa
     }
-    if (!email) {
-        alert("Campo e-mail não cadastrado, informe se e-mail")
-        inputEmail.focus
-        return
+  })
 
-    }
-    if (!senha) {
-        alert("Campo Senha não cadastrado, informe a sua senha")
-        inputSenha.focus
-        return
+  // Caso exista, mostra mensagem de erro com alert
+  if (pessoaEncontrada) {
+    alert('Já existe uma pessoa cadastrada com este email')
+    return
+  }
 
-    }
+  // Caso não exista, adiciona no array listaPessoas
+  listaPessoas.push({
+    nome: nome,
+    email: email,
+    senha: senha
+  })
 
+  // E adiciona na tabela no HTML
 
-    console.log(
-        {
-            nome: nome,
-            email: email,
-            senha: senha,
-        }
-    )
+  const trPessoa = document.createElement("tr")
+  const tdNome = document.createElement("td")
+  tdNome.textContent = nome
+  trPessoa.appendChild(tdNome)
 
+  
+  const tdEmail = document.createElement("td")
+  tdEmail.textContent = email
+  trPessoa.appendChild(tdEmail)
+
+  tableListaPessoas.appendChild(trPessoa)
+}
+
+const cadastrarAnonimo = (event) => {
+  console.log(event)
 }
 
 btnCadastrar.addEventListener('click', cadastrar)
-
